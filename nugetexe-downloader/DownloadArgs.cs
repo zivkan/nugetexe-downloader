7ba9745c1929f8260ca2dd283135fbbb64651fd9
Andy Zivkovic <zivkovic.andy@gmail.com>
2021-10-02 10:10:24 -0700

    Basic command options

---
+++ nugetexe-downloader/DownloadArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace zivkan.nugetexe.downloader
+{
+    internal class DownloadArgs
+    {
+        public DownloadArgs(DirectoryInfo destination, string url)
+        {
+            Destination = destination ?? throw new ArgumentNullException(nameof(destination));
+            Url = string.IsNullOrEmpty(url) ? Program.DefaultUrl : url;
+        }
+
+        public DirectoryInfo Destination { get; set; }
+
+        public bool PreRelease { get; set; }
+
+        public string Url { get; set; }
+
+        public bool DryRun { get; set; }
+    }
+}
--- nugetexe-downloader/Program.cs
+++ nugetexe-downloader/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace nugetexe_downloader
+namespace zivkan.nugetexe.downloader
 {
     internal class Program
     {
-        static void Main(string[] args)
+        internal static readonly string DefaultUrl = "https://dist.nuget.org/tools.json";
+
+        public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootCommand = new RootCommand("Download and manage versions of NuGet.exe");
+
+            var destinationOption = new Option<DirectoryInfo>(new[] { "--destination", "-d" })
+            {
+                Description = "Directory NuGet.exe should be written to",
+                Arity = ArgumentArity.ExactlyOne,IsRequired = true
+            };
+            rootCommand.AddOption(destinationOption);
+
+            var prereleaseOption = new Option<bool>("--prerelease")
+            {
+                Description = "Include prerelease versions"
+            };
+            rootCommand.AddOption(prereleaseOption);
+
+            var urlOption = new Option<string>("--url")
+            {
+                Description = "URL for tools.json. Defaults to " + DefaultUrl
+            };
+            rootCommand.AddOption(urlOption);
+
+            var dryRunOption = new Option<bool>("--dry-run")
+            {
+                Description = "Do not download NuGet.exe, only output what would be performed"
+            };
+            rootCommand.Add(dryRunOption);
+
+            rootCommand.Handler = CommandHandler.Create<DownloadArgs>(DownloadAsync);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        private static async Task<int> DownloadAsync(DownloadArgs args)
+        {
+            Console.WriteLine($"Destination = {args.Destination}");
+            Console.WriteLine($"Prerelease = {args.PreRelease}");
+            Console.WriteLine($"Url = {args.Url} ({args.Url?.Length})");
+
+            return 0;
         }
     }
 }
