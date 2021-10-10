e02ee34ddf103f3e670f459d79761f93d1b3218c
Andy Zivkovic <zivkovic.andy@gmail.com>
2021-10-10 10:59:51 -0700

    Download tools.json

---
+++ nugetexe-downloader/Model/Tool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace zivkan.nugetexe.downloader.Model
+{
+    internal class Tool
+    {
+        public string? version { get; set; }
+
+        public string? url { get; set; }
+
+        public string? stage { get; set; }
+
+        public DateTimeOffset? uploaded { get; set; }
+    }
+}
--- nugetexe-downloader/Program.cs
+++ nugetexe-downloader/Program.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using zivkan.nugetexe.downloader.Model;
 
 namespace zivkan.nugetexe.downloader
 {
@@ -39,16 +45,57 @@ namespace zivkan.nugetexe.downloader
             };
             rootCommand.Add(dryRunOption);
 
-            rootCommand.Handler = CommandHandler.Create<DownloadArgs>(DownloadAsync);
+            rootCommand.Handler = CommandHandler.Create<DownloadArgs>(RunAsync);
 
             return await rootCommand.InvokeAsync(args);
         }
 
-        private static async Task<int> DownloadAsync(DownloadArgs args)
+        private static async Task<int> RunAsync(DownloadArgs args)
         {
-            Console.WriteLine($"Destination = {args.Destination}");
-            Console.WriteLine($"Prerelease = {args.PreRelease}");
-            Console.WriteLine($"Url = {args.Url} ({args.Url?.Length})");
+            using (HttpClient httpClient = new())
+            {
+                var response = await httpClient.GetAsync(args.Url, HttpCompletionOption.ResponseContentRead);
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+                var tools = JsonSerializer.Deserialize<Dictionary<string, List<Tool>>>(json);
+
+                if (!tools.TryGetValue("nuget.exe", out List<Tool>? versions))
+                {
+                    Console.WriteLine("Did not find nuget.exe in " + args.Url);
+
+                    return -1;
+                }
+
+                var workingVersions =
+                    versions.Select(tool => new
+                    {
+                        Version = NuGetVersion.Parse(tool.version),
+                        Url = tool.url
+                    });
+
+                if (!args.PreRelease)
+                {
+                    workingVersions = workingVersions.Where(tool => !tool.Version.IsPrerelease);
+                }
+
+                var sortedVersions = 
+                    workingVersions
+                    .OrderBy(tool => tool.Version)
+                    .ToList();
+
+                if (sortedVersions.Count == 0)
+                {
+                    Console.WriteLine("tools.json did not contain any versions of nuget.exe");
+                    return 0;
+                }
+
+                Console.WriteLine($"Found {sortedVersions.Count} versions of nuget.exe");
+                foreach (var version in sortedVersions)
+                {
+                    Console.WriteLine($"  {version.Version}");
+                }
+            }
 
             return 0;
         }
